1402aa2bd55638279142c1b21c648973b02180fa
Egorka-drug <sashamorozov907@gmail.com>
2026-06-06 23:04:04 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine,String,Integer,Column,DateTime,Boolean,Date
+from sqlalchemy import create_engine, String, Integer, Column, DateTime, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import datetime
@@ -6,24 +6,27 @@ import datetime
 
 DATABASE_Url = 'sqlite:///./admin.db'
 
-engine = create_engine(DATABASE_Url,connect_args={"check_same_thread":False})
-SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+engine = create_engine(DATABASE_Url, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column(Integer,primary_key=True,index=True)
-    telegram_id = Column(String,unique=True,index=True,nullable=False)
-    name = Column(String,nullable=True)
-    register_at = Column(String,nullable=True)
-    active = Column(Boolean,default=True)
-    premium = Column(Boolean,default=False)
+    id = Column(Integer, primary_key=True, index=True)
+    telegram_id = Column(String, unique=True, index=True, nullable=False)
+    name = Column(String, nullable=True)
+    register_at = Column(String, nullable=True)
+    active = Column(Boolean, default=True)
+    premium = Column(Boolean, default=False)  # Premium навсегда, если True
+    crypto_invoice_id = Column(String, nullable=True)  # Для Crypto Bot
+
 
 class BroadCast(Base):
     __tablename__ = 'broadcasts'
-    id = Column(Integer,primary_key=True,index=True)
-    message = Column(String,nullable=False)
-    sent_at = Column(DateTime,default=datetime.datetime.now())
+    id = Column(Integer, primary_key=True, index=True)
+    message = Column(String, nullable=False)
+    sent_at = Column(DateTime, default=datetime.datetime.now())
 
 
 Base.metadata.create_all(bind=engine)
